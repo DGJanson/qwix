3b7f21013f9053035c5b2688d9bed885f1d5a3d6
David Janson <dgjanson@gmail.com>
2021-11-30 17:34:06 +0100

    Finished card

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import logging
 from .game.dice import simulateThrow, getPossibleNumbers
 from .game.card import Card
 
-logger = logging.getLogger("timeaftertime")
+logger = logging.getLogger("qwix")
 
 def setupLogging():
     logging.basicConfig(level = logging.INFO)
@@ -13,16 +13,14 @@ def setupLogging():
 if __name__ == "__main__":
 
     setupLogging()
-    
+
     card = Card()
-    
+
     for i in range(0,10):
         throw = getPossibleNumbers(simulateThrow())
         logger.info(throw)
         if (card.isAvailable("blue", throw["blue"][0]) >= 0):
             card.markNumber("blue", throw["blue"][0])
             logger.info("Marked number {}".format(throw["blue"][0]))
-            
+
     logger.info("Score = {}".format(card.calcScore()))
-    
-    
